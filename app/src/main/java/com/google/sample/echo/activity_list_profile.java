1eb859c4a23994b64dafb3afae1309fa042db58b
tamnuwat <tamnuwat.val@gmail.com>
2018-12-27 02:41:57 +0700

    Version1

---
+++ app/src/main/java/com/google/sample/echo/activity_list_profile.java
@@ -0,0 +1,13 @@
+package com.google.sample.echo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_list_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_profile);
+    }
+}
